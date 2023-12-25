867b8ed3e3d0aef439b5b87ac04b00f14c924c96
tyr1kqq <igorturkinwhite@yandex.ru>
2023-12-25 20:57:38 +0200

    Добавьте файлы проекта.

---
+++ Finaly.Progect.Modul9/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Data;
+
+namespace Finaly.Progect.Modul9
+{
+    class MyException: Exception
+    {
+        public MyException(string Mesage) { }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Exception[] expencion =
+            {
+            new DivideByZeroException(),
+            new FormatException(),
+            new FieldAccessException(),
+            new FileLoadException(),
+            new ArgumentException(),
+            new MyException("Warning")
+            };
+             
+            foreach (var ex in expencion)
+            {
+                try
+                {
+                    throw ex;
+                }
+                catch( DivideByZeroException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch ( FormatException e )
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (FieldAccessException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (FileLoadException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (MyException e)
+                {
+                    Console.WriteLine( "А это мое исключение  " + e.Message);
+                }
+                finally { Console.WriteLine(); }
+            }
+           
+            
+            
+
+            
+        }
+    }
+}
